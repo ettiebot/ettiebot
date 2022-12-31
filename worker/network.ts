12805586bf54d7ff639def9a1c05e39b9fe74f94
1devout0 <1devout0@gmail.com>
2022-12-31 17:48:55 +1000

    revert

--- worker/network.ts
+++ worker/network.ts
@@ -2,18 +2,17 @@ import Translink from "@coryfoundation/translink";
 import { NETWORK_ID } from "./env";
 
 export class Network {
-  public client: Translink;
+  public client = new Translink({
+    namespace: NETWORK_ID,
+    waitForPeer: false,
+    requestTimeout: 180000,
+    log: true,
+  });
 
   /**
    * Initialize network class
    */
   public async start() {
-    this.client = new Translink({
-      namespace: NETWORK_ID,
-      waitForPeer: false,
-      requestTimeout: 180000,
-      log: true,
-    });
     await this.client.connect();
     console.info("Network has been started");
   }
