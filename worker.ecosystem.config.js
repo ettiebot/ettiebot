b3b7b709e4a2aa377ece927ea29576a65c2aa5b6
1devout0 <1devout0@gmail.com>
2023-01-20 14:54:41 +1000

    pm2 eco update

--- worker.ecosystem.config.js
+++ worker.ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'worker',
-      script: 'npm run build && npm run start:worker',
+      script: 'npm run start:worker',
       max_memory_restart: '1G',
     },
   ]
