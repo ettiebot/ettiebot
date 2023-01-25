67c51361ad144a697cf752a6a458c276fea14573
1devout0 <1devout0@gmail.com>
2023-01-26 03:05:24 +1000

    remove build from main

--- build/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cleanHistory = exports.delay = void 0;
-var promiseDelay_util_1 = require("./promiseDelay.util");
-Object.defineProperty(exports, "delay", { enumerable: true, get: function () { return promiseDelay_util_1.delay; } });
-var cleanHistory_util_1 = require("./cleanHistory.util");
-Object.defineProperty(exports, "cleanHistory", { enumerable: true, get: function () { return cleanHistory_util_1.cleanHistory; } });
--- build/utils/promiseDelay.util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.delay = void 0;
-function delay(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time);
-    });
-}
-exports.delay = delay;
