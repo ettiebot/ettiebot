692b9160af7212dc2db57a98e0450cc4e0fef637
1devout0 <1devout0@gmail.com>
2023-02-13 07:51:53 +1000

    fix: clear "алиса" from message text

--- client/utils/clearMessageText.utils.ts
+++ client/utils/clearMessageText.utils.ts
@@ -2,6 +2,7 @@ export default function clearMessageText(message: string): string {
 	// Clear bot mentions
 	const text = message
 		.replace(/\b(Ettie|Etti|Eti|Yetti|Yeti)(,?\s?)/giu, "")
+		.replace(/(алиса|алисочка)(,?\s?)/giu, "")
 		.replace(/(етти|ети|йети|йетти|бот)(,?\s?)/giu, "")
 		.replace(/&#\d+;/g, "")
 		.replace(/&/g, "")
