c5fbcfe78ebd23b95625b07a32a00b2923fd0883
1devout0 <1devout0@gmail.com>
2023-02-12 22:07:36 +1000

    remove image on rebuild

--- inquirer/start.sh
+++ inquirer/start.sh
@@ -1,3 +1,3 @@
-docker stop ettie-inquirer && docker rm ettie-inquirer
+docker stop ettie-inquirer && docker rm ettie-inquirer && docker image rm ettie-client
 docker build -t ettie-inquirer -f ./Dockerfile .
 docker run -d --name ettie-inquirer --network bridge -it ettie-inquirer
