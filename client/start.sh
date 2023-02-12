831fffc355484f4bba49fb4af246177dd9bf9b3b
1devout0 <1devout0@gmail.com>
2023-02-12 22:49:45 +1000

    start.sh update

--- client/start.sh
+++ client/start.sh
@@ -1,3 +1,5 @@
-docker stop ettie-client && docker rm ettie-client && docker image rm ettie-client
+docker stop ettie-client
+docker rm ettie-client
+docker image rm ettie-client
 docker build -t ettie-client -f ./Dockerfile .
 docker run -d --name ettie-client --network bridge -it ettie-client
