a8fd6945d2e1025125fb73cb15d9924154fae69f
1devout0 <1devout0@gmail.com>
2023-02-12 22:49:01 +1000

    start.sh update

--- inquirer/start.sh
+++ inquirer/start.sh
@@ -1,3 +1,5 @@
-docker stop ettie-inquirer && docker rm ettie-inquirer && docker image rm ettie-inquirer
+docker stop ettie-inquirer
+docker rm ettie-inquirer
+docker image rm ettie-inquirer
 docker build -t ettie-inquirer -f ./Dockerfile .
 docker run -d --name ettie-inquirer --network bridge -it ettie-inquirer
